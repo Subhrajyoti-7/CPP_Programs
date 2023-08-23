eb77aa428e27a2a9d6264860c7c12c66af6978f7
Subhrajyoti <subhrajyotibiswal99@gmail.com>
2023-08-23 18:28:35 +0530

    visited one program

--- test.cpp
+++ test.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int a = 10;
-    int b = 6;
-    cout << a ^ b;
-    return (0);
+    int num;
+    int count;
+    int count1;
+    cout << "enter a number : ";
+    cin >> num;
+    int arr[num];
+    int freq[num];
+    int k = 0;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < num; i++)
+    {
+        count = 0;
+        for (int j = 0; j < num; j++)
+        {
+            if (arr[j] == arr[i])
+            {
+                count++;
+            }
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            cout << freq[i] << " ";
+        }
+    }
 }
